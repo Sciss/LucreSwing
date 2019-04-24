1c3e1e85fb6e8127bbc7d4b36a5a16ad455df421
Hanns Holger Rutz <contact@sciss.de>
2019-04-24 15:18:15 +0200

    another trig debug case

--- src/test/scala/de/sciss/lucre/swing/TrigCombinationTest.scala
+++ src/test/scala/de/sciss/lucre/swing/TrigCombinationTest.scala
@@ -1,6 +1,5 @@
 package de.sciss.lucre.swing
 
-import de.sciss.lucre.expr.graph.PrintLn
 import de.sciss.lucre.expr.{Ex, ExOps}
 import de.sciss.lucre.stm.{InMemory, Workspace}
 
@@ -21,24 +20,50 @@ object TrigCombinationTest extends AppLike {
     de.sciss.lucre.event.showLog = true
 
     import ExOps._
-    import graph._
-    val g = Graph {
+    import de.sciss.lucre.expr.graph._
+    import de.sciss.lucre.swing.graph._
+
+//    val g = Graph {
 //      val t1 = Bang()
 //      val t2 = Bang()
 //      val t3 = Bang()
 //      t1 ---> t2
 //      (t1 & t2) ---> t3
 //      FlowPanel(t1, t2, t3)
+//    }
 
-      val a = Button("Click")
-      val b = Bang()
-      val t1 = a.clicked
-      t1 ---> b
-      val t2 = b
-      val t3 = t1 & t2
-      t3 ---> PrintLn("Both")
+//    val g = Graph {
+//      val a = Button("Click")
+//      val b = Bang()
+//      val t1 = a.clicked
+//      t1 ---> b
+//      val t2 = b
+//      val t3 = t1 & t2
+//      t3 ---> PrintLn("Both")
+//
+//      FlowPanel(a, b)
+//    }
 
-      FlowPanel(a, b)
+    val g = Graph {
+      val r     = Bang()
+      val bPlay = Bang()
+      val bStop = Bang()
+
+      val play = r.filter(true)
+      val stop = r.filter(false)
+
+      play ---> bPlay
+      stop ---> bStop
+
+      // DEBUG: only one print should be activated
+      bPlay ---> PrintLn("START")
+      bStop ---> PrintLn("STOP")
+
+      FlowPanel(
+        Label("In:"), r,
+        Label("Play:"), bPlay,
+        Label("Stop:"), bStop
+      )
     }
 
     type              S = InMemory
